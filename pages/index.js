b4cca60c0555c925aa7d5da86a636407c45cda35
Lars Wittenberg <larswittenberg@googlemail.com>
2021-01-29 17:39:56 +0100

    FIX: Demo Image

--- pages/index.js
+++ pages/index.js
@@ -34,12 +34,13 @@ export default function IndexPage() {
 			<h2 className="text-center mb-8 text-2xl">Next.js Image Demo</h2>
 			<figure>
 				<Image
-					src="/images/jonny-caspari-K8bxn2-hLMM-unsplash.jpg"
-					width={1280}
-					height={958}
-					alt="Coastline Sunset Image from Jonny Caspari from Unsplash" />
+					src="/images/unsplash.jpg"
+					width={1920}
+					height={1280}
+					alt="Alpe di Siusi, Italia from Pietro De Grandi from Unsplash"
+				/>
 				<figcaption>
-					<span>Photo by <a href="https://unsplash.com/@jonnysplsh?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Jonny Caspari</a> on <a href="https://unsplash.com/t/nature?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+					<span>Alpe di Siusi, Italia. Photo by <a href="https://unsplash.com/@peter_mc_greats?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Pietro De Grandi</a> on <a href="https://unsplash.com/images/nature?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
 				</figcaption>
 			</figure>
 
