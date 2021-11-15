23f8abe8c4b476d0bfdcf07d2767c285e7244b88
Lars Wittenberg <larswittenberg@googlemail.com>
2021-11-16 00:42:47 +0100

    UPDATE: Add & Document

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
-
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-					<link href="/favicon.svg" rel="shortcut icon" />
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+export default function Document(props) {
+	return (
+		<Html lang="de">
+			<Head>
+				<link href="/favicon.svg" rel="shortcut icon" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
-
-export default MyDocument
