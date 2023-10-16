db155f1b0c3ea1cdcd380c1eb09495ab0f4e69ef
Lars Wittenberg <larswittenberg@googlemail.com>
2023-10-16 18:46:43 +0200

    BUILD: Add typescript

--- next.config.js
+++ next.config.js
@@ -11,10 +11,13 @@ const withMDX = require('@next/mdx')({
 	},
 })
 
-module.exports = withMDX({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 	// https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
 	reactStrictMode: true,
+	// Configure `pageExtensions`` to include MDX files
+	pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+	// Optionally, add any other Next.js config below
+}
 
-	// Append the default value with md extensions
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+module.exports = withMDX(nextConfig)
