59a8c468d043248ca08481fe69715cd275b64d7f
Lars Wittenberg <larswittenberg@googlemail.com>
2023-11-07 17:02:26 +0100

    UPDATE: Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
+// https://nextjs.org/docs/app/building-your-application/styling/tailwind-css
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,jsx,ts,tsx,mdx}'],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,jsx,ts,tsx,mdx}'
+	],
 	theme: {
 		extend: {
 			colors: {
