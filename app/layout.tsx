1027dc4f6817d703dd34867f4e03e26109ecc960
Lars Wittenberg <larswittenberg@googlemail.com>
2024-01-18 18:36:17 +0100

    FEATURE: Add module path aliases

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import '../styles/main.css';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const roboto = Roboto({
 	weight: ['400', '700'],
