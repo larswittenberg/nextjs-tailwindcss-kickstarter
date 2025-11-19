30a96510fe6d48b1c8cdbb5828b911725860f609
Lars Wittenberg <larswittenberg@googlemail.com>
2025-11-19 23:41:30 +0100

    feat: delete i18n not found page

--- src/app/[lang]/[...not_found]/page.tsx
+++
@@ -1,6 +0,0 @@
-// https://www.linkedin.com/pulse/add-custom-404-page-found-nextjs-14-app-directory-ali-mirzaei-hyh1e/
-import { notFound } from 'next/navigation';
-
-export default function NotFoundCatchAll() {
-	notFound();
-}
